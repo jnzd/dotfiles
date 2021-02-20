4f1c0dc2a39b5798055cd9813d883701abe00de3
jonasdegelo <jonas.degelo@gmail.com>
2021-02-20 20:43:33 +0100

    add ahk script to startup folder

--- windows/install.bat
+++ windows/install.bat
@@ -4,3 +4,6 @@ mklink _vimrc %~dp0_vimrc
 cd C:\tools\neovim\Neovim\share\nvim
 rm sysinit.vim
 mklink sysinit.vim %~dp0sysinit.vim
+cd "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+rm macros.ahk
+mklink macros.ahk %~dp0macros.ahk
