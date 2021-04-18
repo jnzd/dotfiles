ea6b99caeb93f0a909cccaedfa8cd81ca20cf253
Jonas Degelo <degeloj@student.ethz.ch>
2021-04-18 16:57:21 +0200

    replaced rm (which only works because of git) with the natvie CMD command del

--- install.bat
+++ install.bat
@@ -1,14 +1,14 @@
 cd C:\tools\vim
-rm _vimrc
+del _vimrc
 mklink _vimrc %~dp0\windows\vimrc
-rm vimrc
+del vimrc
 mklink vimrc %~dp0\windows\vimrc
 cd C:\Users\%username%\vimfiles
-rm vimrc
+del vimrc
 mklink vimrc %~dp0\windows\vimrc
 cd C:\tools\neovim\Neovim\share\nvim
-rm sysinit.vim
+del sysinit.vim
 mklink sysinit.vim %~dp0\windows\sysinit.vim
 cd "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-rm macros.ahk
+del macros.ahk
 mklink macros.ahk %~dp0\windows\macros.ahk
