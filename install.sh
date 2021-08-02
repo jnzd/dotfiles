d5c8a1c7372059f4230f79e1cb6ea3eca1c53ba6
Jonas <26679346+jonasdegelo@users.noreply.github.com>
2021-08-02 22:50:09 +0200

    execute permissions on install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-ln -s -f ~/.dotfiles/.config/vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.config/vimrc ~/.vimrc
 mkdir -p ~/.config/nvim
-ln -s -f ~/.dotfiles/.config/vimrc ~/.config/nvim/init.vim
-ln -s -f ~/.dotfiles/.config/i3config ~/.i3/config
+ln -s -f ~/dotfiles/.config/vimrc ~/.config/nvim/init.vim
+ls ~/.i3 && ln -s -f ~/dotfiles/.config/i3config ~/.i3/config
