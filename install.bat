50c70cd9f3cf31719fd68048c5cbc3c222d09536
Jonas Degelo <26679346+jonasdegelo@users.noreply.github.com>
2021-08-17 20:46:33 +0200

    fixed install.bat if statements; added dotfiles variable for path to script

--- install.bat
+++ install.bat
@@ -5,37 +5,32 @@ set vim=C:\tools\vim
 set nvim=C:\tools\neovim\Neovim\share\nvim
 set start="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
+:: %~dp0 is the script directory
+:: the :~0,-1% removes the trailing backslash using substrings
+set dotfiles=%~dp0
+set dotfiles=%dotfiles:~0,-1%
+
 :: set up vim
-if exist %vim% ^
-(
-  if not exist %vimtmp% ^
-  (
-    mkdir %vimtmp%
-  )
+if exist %vim% (^
+  if not exist %vimtmp% mkdir %vimtmp%
   if exist %vim%\_vimrc del %vim%\_vimrc
   if exist %vim%\vimrc del %vim%\vimrc
-  :: %~dp0 is the script directory
-  mklink %vim%\_vimrc %~dp0\vimrc
-  mklink %vim%\vimrc %~dp0\vimrc
-  if exist %vimfiles ^
-  (
+  mklink %vim%\_vimrc %dotfiles%\vimrc
+  mklink %vim%\vimrc %dotfiles%\vimrc
+  if exist %vimfiles (^
     cd %vimfiles%
     del vimrc
-    mklink vimrc %~dp0\vimrc
+    mklink vimrc %dotfiles%\vimrc
   )
 )
 
 :: set up neovim
-if exist %nvim% ^
-(
-  if not exist %vimtmp% ^
-  (
-    mkdir %vimtmp%
-  )
+if exist %nvim% (^
+  if not exist %vimtmp% mkdir %vimtmp%
   del %nvim%\sysinit.vim
-  mklink %nvim%\sysinit.vim %~dp0\vimrc
+  mklink %nvim%\sysinit.vim %dotfiles%\vimrc
 )
 
 :: add ahk script to autostart
 if exist %start%\macros.ahk del %start%\macros.ahk
-mklink %start%\macros.ahk %~dp0\windows\macros.ahk
+mklink %start%\macros.ahk %dotfiles%\windows\macros.ahk
