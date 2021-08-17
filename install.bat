d9bf8c66da23889f140fdea139aa242d82c0859d
Jonas Degelo <26679346+jonasdegelo@users.noreply.github.com>
2021-08-17 20:08:28 +0200

    added ~/vimtmp creation to install.bat and rewrote install.bat with checks for existing directories

--- install.bat
+++ install.bat
@@ -1,14 +1,41 @@
-cd C:\tools\vim
-del _vimrc
-mklink _vimrc %~dp0\vimrc
-del vimrc
-mklink vimrc %~dp0\vimrc
-cd C:\Users\%username%\vimfiles
-del vimrc
-mklink vimrc %~dp0\vimrc
-cd C:\tools\neovim\Neovim\share\nvim
-del sysinit.vim
-mklink sysinit.vim %~dp0\vimrc
-cd "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-del macros.ahk
-mklink macros.ahk %~dp0\windows\macros.ahk
+set home=C:\Users\%username%
+set vimtmp=%home%\vimtmp
+set vimfiles=%home%\vimfiles
+set vim=C:\tools\vim
+set nvim=C:\tools\neovim\Neovim\share\nvim
+set start="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+:: set up vim
+if exist %vim% ^
+(
+  if not exist %vimtmp% ^
+  (
+    mkdir %vimtmp%
+  )
+  if exist %vim%\_vimrc del %vim%\_vimrc
+  if exist %vim%\vimrc del %vim%\vimrc
+  :: %~dp0 is the script directory
+  mklink %vim%\_vimrc %~dp0\vimrc
+  mklink %vim%\vimrc %~dp0\vimrc
+  if exist %vimfiles ^
+  (
+    cd %vimfiles%
+    del vimrc
+    mklink vimrc %~dp0\vimrc
+  )
+)
+
+:: set up neovim
+if exist %nvim% ^
+(
+  if not exist %vimtmp% ^
+  (
+    mkdir %vimtmp%
+  )
+  del %nvim%\sysinit.vim
+  mklink %nvim%\sysinit.vim %~dp0\vimrc
+)
+
+:: add ahk script to autostart
+if exist %start%\macros.ahk del %start%\macros.ahk
+mklink %start%\macros.ahk %~dp0\windows\macros.ahk
