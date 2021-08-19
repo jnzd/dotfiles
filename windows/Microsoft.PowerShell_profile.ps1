f5f53d7f1d16082effebbdf9228845b964d21d37
Jonas Degelo <26679346+jonasdegelo@users.noreply.github.com>
2021-08-19 23:55:16 +0200

    removed oh my posh

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,11 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt Zash
-Set-Alias -Name vi -Value nvim
-Set-Alias -Name vim -Value gvim
-Set-Alias -Name vimdiff -Value gvimdiff
-# PSFzf, they should already be installed, but aren't
-Set-Alias -name fe -Value Invoke-FuzzyEdit
-Set-Alias -name ff -Value Invoke-FuzzyFasd
-Set-Alias -name fz -Value Invoke-FuzzyZLocation
-Set-Alias -name fgs -Value Invoke-FuzzyGitStatus
-Set-Alias -name fh -Value Invoke-FuzzyHistory
-Set-Alias -name fkill -Value Invoke-FuzzyKillProcess
-Set-Alias -name fd -Value Invoke-FuzzySetLocation
-Set-Alias -name cde -Value Set-LocationFuzzyEverything
+##### FZF #####
+Enable-PsFzfAliases
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+# replace tab expansion with fzf
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+##### #####
 
+##### my functions #####
 function eth { set-location ~/onedrive/documents/eth }
 
 function search {
@@ -33,7 +25,15 @@ function upgrade {
 
 function disks { GET-WMIOBJECT -query "SELECT * from Win32_DiskDrive" }
 Invoke-Expression (&scoop-search --hook)
+##### #####
 
+##### my aliases #####
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name vim -Value gvim
+Set-Alias -Name vimdiff -Value gvimdiff
+##### #####
+
+##### VIM mode #####
 Set-PSReadLineOption -EditMode vi -viModeIndicator Cursor
 
 function OnViModeChange {
@@ -45,13 +45,11 @@ function OnViModeChange {
         Write-Host -NoNewLine "`e[5 q"
     }
 }
-
 Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange 
+##### #####
+
 Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
 
-# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-# replace tab expansion with fzf
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-# comment added in PowerShell 
-# comment added in PowerShell Preview
+$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+$ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
+Invoke-Expression (&starship init powershell)
