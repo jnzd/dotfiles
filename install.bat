66ff8715492b6865a861d1561501957c5d873cea
Jonas Degelo <26679346+jonasdegelo@users.noreply.github.com>
2021-08-20 00:40:56 +0200

    changed install.bat comments from :: to use REM

--- install.bat
+++ install.bat
@@ -5,12 +5,12 @@ set vim=C:\tools\vim
 set nvim=C:\tools\neovim\Neovim\share\nvim
 set start="C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
-:: %~dp0 is the script directory
-:: the :~0,-1% removes the trailing backslash using substrings
+REM %~dp0 is the script directory
+REM the :~0,-1% removes the trailing backslash using substrings
 set dotfiles=%~dp0
 set dotfiles=%dotfiles:~0,-1%
 
-:: set up vim
+REM set up vim
 if exist %vim% (^
   if not exist %vimtmp% mkdir %vimtmp%
   if exist %vim%\_vimrc del %vim%\_vimrc
@@ -23,13 +23,13 @@ if exist %vim% (^
   )
 )
 
-:: set up neovim
+REM set up neovim
 if exist %nvim% (^
   if not exist %vimtmp% mkdir %vimtmp%
   if exist %nvim%\sysinit.vim del %nvim%\sysinit.vim
   mklink %nvim%\sysinit.vim %dotfiles%\vimrc
 )
 
-:: add ahk script to autostart
+REM add ahk script to autostart
 if exist %start%\macros.ahk del %start%\macros.ahk
 mklink %start%\macros.ahk %dotfiles%\windows\macros.ahk
